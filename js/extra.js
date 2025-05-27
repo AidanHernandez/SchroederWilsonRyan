7e5b4d1da25c5d7e9b45dffecee3d8dc5ad2dd48
Aidan Hernandez <118579227+AidanHernandez@users.noreply.github.com>
2025-05-26 21:27:19 -0700

    work

--- js/extra.js
+++ js/extra.js
@@ -1,5 +1,9 @@
 const playerOne = document.getElementById("readyPlayerOne")
+const playerTwo = document.getElementById("readyPlayerOne")
+
 const wasd = document.getElementById("wasd")
+
+const arrow = document.getElementById("arrow")
 
 
 const homePage = document.getElementById("homePage")
@@ -18,35 +22,75 @@ characterSelectMenu.style.height = menuSize
 
 let characters = ['thimble', 'tony', 'basketball god', 'hatsune miku', 'teddie']
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
     if (event.key === 'w') {
-        if(wasd.classList.contains("wasdOnline")){
-            
+        if (wasd.classList.contains("wasdOnline")) {
+
+            playerOne.classList.add("playerOneSelected")
+            playerOne.classList.remove("showPlayerOne")
             wasd.classList.add("wasdSelected")
-            
+
         }
-        else if(characterSelectMenu.classList.contains("show")){
+        else if (characterSelectMenu.classList.contains("show")) {
             playerOne.src = "../img/characters/thimble.png"
+            playerOne.classList.add("showPlayerOne")
+
+
+
             wasd.classList.add("wasdOnline")
-            
+            wasd.classList.remove("notOnline")
+
         }
-      
-     
+
+
     }
 
     if (event.key === 's') {
-        if(wasd.classList.contains("wasdSelected")){
-            
+        if (wasd.classList.contains("wasdSelected")) {
+            playerOne.classList.remove("playerOneSelected")
+             playerOne.classList.add("showPlayerOne")
             wasd.classList.remove("wasdSelected")
-            
+
         }
-        else if(characterSelectMenu.classList.contains("show")){
+        else if (characterSelectMenu.classList.contains("show")) {
             playerOne.src = "../img/characters/placeholder.png"
+
+            playerOne.classList.remove("showPlayerOne")
+
+
             wasd.classList.remove("wasdOnline")
-            
+            wasd.classList.add("notOnline")
         }
-      
-     
+
+
     }
-  });
+
+    if (event.key === 'ArrowUp') {
+        
+        if (arrow.classList.contains("arrowPlayerSelectingCharacter")) {
+
+            
+            arrow.classList.add("arrowPlayerLocked")
+
+        }
+        else if (characterSelectMenu.classList.contains("show")) {
+            
+
+
+
+            arrow.classList.add("arrowPlayerSelectingCharacter") 
+            arrow.classList.remove("arrowNoPLayer")
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+});
 
