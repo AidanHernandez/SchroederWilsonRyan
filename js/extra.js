9acf9dbd1533cae8ab374aa940e3df9f42072738
Aidan Hernandez <118579227+AidanHernandez@users.noreply.github.com>
2025-05-29 09:19:52 -0700

    character select done

--- js/extra.js
+++ js/extra.js
@@ -1,8 +1,13 @@
-const playerOne = document.getElementById("readyPlayerOne")
-const playerTwo = document.getElementById("readyPlayerTwo")
+const playerOne = document.getElementById("readyPlayerOne");
+const playerTwo = document.getElementById("readyPlayerTwo");
+const playerThree = document.getElementById("readyPlayerThree");
+const playerFour = document.getElementById("readyPlayerFour");
 
-const wasd = document.getElementById("wasd")
-const arrow = document.getElementById("arrow")
+const wasd = document.getElementById("wasd");
+const arrow = document.getElementById("arrow");
+const tfgh = document.getElementById("tfgh");
+const ijkl = document.getElementById("ijkl");
+
 
 
 const homePage = document.getElementById("homePage")
@@ -20,234 +25,150 @@ characterSelectMenu.style.height = menuSize
 
 
 let characters = [
-    ['thimble', 'tony', 'basketballGod', 'hatsune miku', 'teddie'],
-    ['thimble', 'tony', 'basketballGod', 'hatsune miku', 'teddie']
+    ['thimble', 'tony', 'basketballGod', 'hatsuneMiku', 'teddie'],
+    ['thimble', 'tony', 'basketballGod', 'hatsuneMiku', 'teddie']
 ]
 
-let playerOptions = []
+let playerOptions = ["", "", "", ""];
+let playerIndexes = [0, 0, 0, 0];
 
-let playerIndexes = [0, 0];
 
-let characterNumber = 0;
+const players = [playerOne, playerTwo, playerThree, playerFour];
+const controls = [wasd, arrow, tfgh, ijkl];
+
+
 
 
 document.addEventListener('keydown', function (event) {
-
-
     switch (event.key) {
-        case 'w':
-            if (wasd.classList.contains("wasdOnline")) {
 
-                playerOne.classList.add("playerOneSelected")
-                playerOne.classList.remove("showPlayerOne")
-
-                wasd.classList.add("wasdSelected")
-
-            }
-            else if (characterSelectMenu.classList.contains("show")) {
-
-                showCharacter(playerOne)
+        case 'w': handleJoinOrLock(0); break;
+        case 'a': handleScroll(0, -1); break;
+        case 'd': handleScroll(0, 1); break;
+        case 's': handleUnjoinOrUnlock(0); break;
 
 
-                playerOne.classList.add("showPlayerOne")
+        case 'ArrowUp': handleJoinOrLock(1); break;
+        case 'ArrowLeft': handleScroll(1, -1); break;
+        case 'ArrowRight': handleScroll(1, 1); break;
+        case 'ArrowDown': handleUnjoinOrUnlock(1); break;
 
 
-
-                wasd.classList.add("wasdOnline")
-                wasd.classList.remove("notOnline")
-
-            }
-            break;
-        case 'd':
-            if (playerOne.classList.contains("showPlayerOne")) {
-
-                characterScroll(playerOne)
-            }
-            break;
-        case 's':
-            if (wasd.classList.contains("wasdSelected")) {
-                playerOne.classList.remove("playerOneSelected")
-                playerOne.classList.add("showPlayerOne")
+        case 't': handleJoinOrLock(2); break;
+        case 'f': handleScroll(2, -1); break;
+        case 'h': handleScroll(2, 1); break;
+        case 'g': handleUnjoinOrUnlock(2); break;
 
 
-                wasd.classList.remove("wasdSelected")
+        case 'i': handleJoinOrLock(3); break;
+        case 'j': handleScroll(3, -1); break;
+        case 'l': handleScroll(3, 1); break;
+        case 'k': handleUnjoinOrUnlock(3); break;
 
-            }
-            else if (characterSelectMenu.classList.contains("show")) {
+        case ' ': if(canStartGame()){console.log("game screen next")} break;
 
-
-                removeCharacter(playerOne)
-                playerOne.classList.remove("showPlayerOne")
-
-
-                wasd.classList.remove("wasdOnline")
-                wasd.classList.add("notOnline")
-            }
-            break;
-        case 'ArrowUp':
-            if (arrow.classList.contains("arrowPlayerSelectingCharacter")) {
-
-                arrow.classList.add("arrowPlayerLocked")
-
-                playerTwo.classList.add("playerTwoSelected")
-                playerTwo.classList.remove("showplayerTwo")
-
-            }
-            else if (characterSelectMenu.classList.contains("show")) {
-
-                showCharacter(playerTwo)
-
-                //shows the player
-                playerTwo.classList.add("showplayerTwo")
-
-                //arrowkeys
-                arrow.classList.add("arrowPlayerSelectingCharacter")
-                arrow.classList.remove("arrowNoPLayer")
-
-            }
-            break;
-        case 'ArrowDown':
-            if (arrow.classList.contains("arrowPlayerLocked")) {
-                arrow.classList.remove("arrowPlayerLocked")
-
-                playerTwo.classList.remove("playerTwoSelected")
-                playerTwo.classList.add("showplayerTwo")
-
-            }
-            else if (characterSelectMenu.classList.contains("show")) {
-
-                removeCharacter(playerTwo)
-
-                playerTwo.classList.remove("showplayerTwo")
-
-                arrow.classList.remove("arrowPlayerSelectingCharacter")
-                arrow.classList.add("arrowNoPLayer")
-            }
-            break;
-        case 'ArrowRight':
-            if (playerTwo.classList.contains("showplayerTwo")) {
-
-                characterScroll(playerTwo)
-            }
-            break;
     }
-
-
-
 });
 
 
 
-function showCharacter(player) {
 
+function handleJoinOrLock(index) {
+    const player = players[index];
+    const control = controls[index];
 
-    let character = characters[1][0]
-
-
-
-
-    showCharacterByName(player, character)
-
-
-
-    switch (player) {
-        case playerOne:
-            playerOptions[0] = character
-
-            break;
-        case playerTwo:
-            playerOptions[1] = character
-            break;
-        // case playerOne:
-        //     playerOptions[0] = character
-        //     break;
-        // case playerOne:
-        //     playerOptions[0] = character
-        //     break;
-
+    if (!player.classList.contains("showPlayer")) {
+        showCharacter(player);
+        player.classList.add("showPlayer");
+        control.style.opacity = 0.5;
+        control.classList.add("online");
+        control.classList.remove("notOnline");
+    } else {
+        player.classList.add("selected");
+        control.classList.remove("online");
+        control.classList.add("locked");
+        control.style.opacity = 1;
     }
 
-    for (i = characters[1].length; i >= 0; i--) {
+    if (canStartGame()) {
 
-        if (characters[1][i] === character) {
-            characters[1].splice(i, 1)
-        }
+        document.getElementById("characterSelectMenuText").innerText = "Press Space To Start"
+
     }
-
-    console.log(characters)
-
+    else {
+        document.getElementById("characterSelectMenuText").innerText = "Choose Your Character"
+    }
 }
+
+
+
+
+function handleUnjoinOrUnlock(index) {
+    const player = players[index];
+    const control = controls[index];
+
+    if (control.classList.contains("locked")) {
+        control.classList.remove("locked");
+        control.classList.add("online");
+        player.classList.remove("selected");
+        control.style.opacity = 0.5;
+    } else {
+        removeCharacter(player);
+        player.classList.remove("showPlayer");
+        control.classList.remove("online");
+        control.classList.add("notOnline");
+        control.style.opacity = 1;
+        removeColors(control);
+    }
+
+    if (canStartGame()) {
+        console.log("Ready to start the game!");
+        // Optionally call startGame(); or enable a "Start" button here
+    }
+}
+
+
+
+
+function handleScroll(index, direction) {
+    const player = players[index];
+    const control = controls[index];
+
+
+    if (player.classList.contains("showPlayer") && !control.classList.contains("locked")) {
+        characterScroll(player, direction);
+    }
+}
+
+
+
+
+function showCharacter(player) {
+    let index = players.indexOf(player);
+    let character = characters[1][0];
+    showCharacterByName(player, character);
+    playerOptions[index] = character;
+
+    characters[1] = characters[1].filter(c => c !== character);
+}
+
+
+
+
 
 function removeCharacter(player) {
-
-
-    switch (player) {
-        case playerOne:
-            playerOptions[0] = ""
-
-            break;
-        case playerTwo:
-            playerOptions[1] = ""
-
-            break;
-    }
-
-    addPlayer()
-
-    player.src = "../img/characters/placeholder.png"
+    let index = players.indexOf(player);
+    playerOptions[index] = "";
+    addPlayer();
+    player.src = "../img/characters/placeholder.png";
 }
 
-function showCharacterByName(player, character) {
-    switch (character) {
-        case 'thimble':
 
-            player.src = "../img/characters/thimble.png";
-            switch (player) {
-                case playerOne:
-                    removeColors(wasd)
-                    wasd.classList.add("thimble")
-
-
-                break;
-            }
-            break;
-        case 'tony':
-            player.src = "../img/characters/tony.png";
-            if (player = playerOne) {
-                wasd.classList.add("tony")
-            }
-            break;
-        case 'basketballGod':
-            player.src = "../img/characters/basketballgod.png";
-            if (player = playerOne) {
-                wasd.classList.add("basketball")
-            }
-            break;
-        case 'hatsune miku':
-            player.src = "../img/characters/miku.png";
-            if (player = playerOne) {
-                wasd.classList.add("miku")
-            }
-            break;
-        case 'teddie':
-            player.src = "../img/characters/teddie.png";
-            if (player = playerOne) {
-                wasd.classList.add("teddie")
-            }
-            break;
-        default:
-            player.src = "../img/characters/placeholder.png";
-            break;
-    }
-}
 
 
 
 function addPlayer() {
-    // Clear current list of available characters
     characters[1] = [];
-
-    // Re-add characters from the master list, excluding ones currently selected by players
     for (let i = 0; i < characters[0].length; i++) {
         const character = characters[0][i];
         if (!playerOptions.includes(character)) {
@@ -257,41 +178,86 @@ function addPlayer() {
 }
 
 
-function characterScroll(player) {
-    let playerIndex = (player === playerOne) ? 0 : 1;
-    let currentCharacter = playerOptions[playerIndex];
 
-    // Rebuild the character pool with everything
+
+
+function characterScroll(player, direction = 1) {
+
+
+
+    let index = players.indexOf(player);
+    let currentCharacter = playerOptions[index];
+
     addPlayer();
-
-    // Remove the currently selected character from pool
     let currentIndex = characters[1].indexOf(currentCharacter);
-    if (currentIndex !== -1) {
-        characters[1].splice(currentIndex, 1);
-    }
+    if (currentIndex !== -1) characters[1].splice(currentIndex, 1);
 
-    // Find the next character that's not taken
     let attempts = 0;
+    let nextCharacter;
     do {
-        playerIndexes[playerIndex] = (playerIndexes[playerIndex] + 1) % characters[0].length;
-        var nextCharacter = characters[0][playerIndexes[playerIndex]];
+        playerIndexes[index] = (playerIndexes[index] + direction + characters[0].length) % characters[0].length;
+        nextCharacter = characters[0][playerIndexes[index]];
         attempts++;
     } while (playerOptions.includes(nextCharacter) && attempts < characters[0].length);
 
-    // Assign new character
-    playerOptions[playerIndex] = nextCharacter;
+    playerOptions[index] = nextCharacter;
     showCharacterByName(player, nextCharacter);
-
-    // Refresh character pool without duplicates
     addPlayer();
 }
 
-['thimble', 'tony', 'basketballGod', 'hatsune miku', 'teddie']
 
-function removeColors(keys){
-    for(i = 0; i < characters[0].length; i++){
-        keys.classList.remove("thimble")
+
+function showCharacterByName(player, character) {
+    const index = players.indexOf(player);
+    const control = controls[index];
+
+    switch (character) {
+        case 'thimble':
+            player.src = "../img/characters/thimble.png";
+            break;
+        case 'tony':
+            player.src = "../img/characters/tony.png";
+            break;
+        case 'basketballGod':
+            player.src = "../img/characters/basketballgod.png";
+            break;
+        case 'hatsuneMiku':
+            player.src = "../img/characters/miku.png";
+            break;
+        case 'teddie':
+            player.src = "../img/characters/teddie.png";
+            break;
+        default:
+            player.src = "../img/characters/placeholder.png";
+            break;
     }
-    
-    
+
+    removeColors(control);
+    control.classList.add(character);
+}
+
+
+
+function removeColors(control) {
+    for (let i = 0; i < characters[0].length; i++) {
+        control.classList.remove(characters[0][i]);
+    }
+}
+
+function canStartGame() {
+    let lockedCount = 0;
+    let choosing = 0
+    for (let i = 0; i < controls.length; i++) {
+        if (controls[i].classList.contains("locked")) {
+            lockedCount++;
+        }
+        if (controls[i].classList.contains("online")) {
+            choosing++
+        }
+    }
+
+    if (lockedCount >= 2 && choosing === 0) {
+        return true
+    }
+
 }
